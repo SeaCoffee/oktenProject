dd718fef6082dcfc8cbcfb4d0d751db99fb2e70a
SeaCoffee <sapienti8sat@gmail.com>
2023-05-17 08:37:25 +0300

    ver4 with dockerfiles

--- autoria_clone/autoria_clone/settings.py
+++ autoria_clone/autoria_clone/settings.py
@@ -77,10 +77,15 @@ WSGI_APPLICATION = 'autoria_clone.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'foroktenproject',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': 'mysql://localhost:3306',
+        'PORT': '3306',
     }
 }
+
 
 
 # Password validation
